564ea6448a28a038545366119397a1fc5fad0d84
jamie-ubuntu <jamiesullivan523@gmail.com>
2020-01-13 13:09:21 +0000

    Initial version of jhipster_gallery generated by JHipster-6.6.0

---
+++ src/main/java/com/jsullivan/gallery/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.jsullivan.gallery.config.audit;
---
+++ src/main/java/com/jsullivan/gallery/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.jsullivan.gallery.repository;
+
+import com.jsullivan.gallery.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
